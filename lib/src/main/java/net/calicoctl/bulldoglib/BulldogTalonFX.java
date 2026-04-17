094046260908bcf5b4999c84f1c4fad01d9eb9db
Ian Galaviz <brianatthehouse@gmail.com>
2026-04-17 15:22:05 -0600

    adjustments to build.gradles and remove unnecessary imports

--- lib/src/main/java/net/calicoctl/bulldoglib/BulldogTalonFX.java
+++ lib/src/main/java/net/calicoctl/bulldoglib/BulldogTalonFX.java
@@ -1,8 +1,6 @@
 package net.calicoctl.bulldoglib;
 
 import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
