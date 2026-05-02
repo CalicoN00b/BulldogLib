ee52f646b277e240a4182e864b4436a04360c31e
Ian Galaviz <brianatthehouse@gmail.com>
2026-05-01 19:09:43 -0600

    javadocced BulldogDeviceManager

--- bulldoglib/src/main/java/net/calicoctl/bulldoglib/control/BulldogDeviceManager.java
+++ bulldoglib/src/main/java/net/calicoctl/bulldoglib/control/BulldogDeviceManager.java
@@ -2,10 +2,20 @@ package net.calicoctl.bulldoglib.control;
 
 import java.util.HashMap;
 
+/**
+ * Class for managing every Bulldog device.
+ * To manage a Bulldog device, it must first be registered into the device manager,
+ * and then all registered devices can be updating using {@link #updateRegisteredDevices}
+ */
 public class BulldogDeviceManager {
 
     private static HashMap<Integer, LoggableMotor> registeredMotors = new HashMap<>();
 
+    /**
+     * Registers a LoggableMotor to the device manager.
+     * Registering a motor allows it to be included in {@link #updateRegisteredDevices}.
+     * @param motor
+     */
     public static void registerMotor(LoggableMotor motor) {
         if (registeredMotors.containsKey(motor.getID())) {
             throw new IllegalArgumentException(String.format("Cannot register %s, as %s is already registered to CAN ID $d!", motor.getName(), registeredMotors.get(motor.getID()), motor.getID()));
@@ -13,7 +23,13 @@ public class BulldogDeviceManager {
         registeredMotors.put(motor.getID(), motor);
     }
 
-    public static void updateAllDevices() {
+    /**
+     * Updates all registered devices.
+     * @see LoggableMotor#update
+     * @see BulldogTalonFX#update
+     * @see BulldogSparkFlex#update
+     */
+    public static void updateRegisteredDevices() {
         for (LoggableMotor motor : registeredMotors.values()) {
             motor.update();
         }
